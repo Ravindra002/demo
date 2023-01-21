2da0ca02da846d0c7188d700ccd0a4e56696b8ee
Ravindra002 <123241244+Ravindra002@users.noreply.github.com>
2023-01-21 19:24:21 +0530

    Update sql-servername.ps1

--- sql-servername.ps1
+++ sql-servername.ps1
@@ -1,8 +1,7 @@
 $sqlserver = 'sql-server\SQLEXPRESS'
 $database = 'test'
-
 Try{
-    invoke-sqlcmd -U ravi -P Freak@502 -ServerInstance "$sqlserver" -Query "Drop database $database;"
+    Invoke-Sqlcmd -U ravi -P Freak@502 -ServerInstance "$sqlserver" -Query "Drop database $database;"
 }Catch{
       Write-Output 'Failed to delete database'
 }
