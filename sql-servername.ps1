524c59dd5c5f78785df082f7a4b0481e84c66b36
Ravindra002 <123241244+Ravindra002@users.noreply.github.com>
2023-01-22 13:46:18 +0530

    Update sql-servername.ps1

--- sql-servername.ps1
+++ sql-servername.ps1
@@ -1,7 +1,20 @@
-$sqlserver = '10.0.0.10'
-$database = 'test'
-Try{
-    invoke-sqlcmd -ServerInstance "$sql-server" -U ravi -P Freak@502 -Query "Drop database $database;"
-}Catch{
-      Write-Output 'Failed to delete database'
-}
+$SqlServer    = 'SQL-SERVER' # SQL Server instance (HostName\InstanceName for named instance)
+$Database     = 'test'      # SQL database to connect to 
+$SqlAuthLogin = 'ravi'          # SQL Authentication login
+$SqlAuthPw    = 'Freak@502'     # SQL Authentication login password
+# query to show changes
+$Query = '
+SELECT @@SERVERNAME AS [ServerName]
+    , des.login_name
+    , DB_NAME()   AS [DatabaseName]
+    , dec.net_packet_size
+    , @@LANGUAGE  AS [Language]
+    , des.program_name
+    , des.host_name
+FROM sys.dm_exec_connections dec
+JOIN sys.dm_exec_sessions des ON dec.session_id = des.session_id
+WHERE dec.session_id = @@SPID
+'
+
+# sql authentication without database name
+Invoke-Sqlcmd -ServerInstance $SqlServer -U $SqlAuthLogin -P $SqlAuthPw -Query $Query | Format-Table
