27649b20ad5b8f2c122d00d95ab8b63d05339f95
Ravindra002 <123241244+Ravindra002@users.noreply.github.com>
2023-01-23 16:52:22 +0530

    Update dropdatabase.ps1

--- dropdatabase.ps1
+++ dropdatabase.ps1
@@ -1,13 +1,10 @@
-$SqlServer    = 'SQL-SERVER' # SQL Server instance (HostName\InstanceName for named instance)
 $Database     = 'test'      # SQL database to connect to 
 $SqlAuthLogin = 'ravi'          # SQL Authentication login
 $SqlAuthPw    = 'Freak@502'     # SQL Authentication login password
 #drop database
-Try{
+
 $sqlservers = Get-Content -Path "C:\Users\ravi\Desktop\sql-servers.txt"
 Foreach($server in $sqlservers) {
-Invoke-Sqlcmd -ServerInstance $SqlServer -U $SqlAuthLogin -P $SqlAuthPw -Query "Drop database $Database;"
+Invoke-Sqlcmd -ServerInstance $server -U $SqlAuthLogin -P $SqlAuthPw -Query "Drop database $Database;"
 }
-}Catch{
-      Write-Output 'Failed to delete database'
-}
+
