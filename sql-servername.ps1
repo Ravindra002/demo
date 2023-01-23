0e2bf1431b4d095f050089ebbd7314e3099465e4
Ravindra002 <123241244+Ravindra002@users.noreply.github.com>
2023-01-23 16:52:57 +0530

    Update sql-servername.ps1

--- sql-servername.ps1
+++ sql-servername.ps1
@@ -1,4 +1,4 @@
-$SqlServer    = 'SQL-SERVER' # SQL Server instance (HostName\InstanceName for named instance)
+#$SqlServer    = 'SQL-SERVER' # SQL Server instance (HostName\InstanceName for named instance)
 $Database     = 'test'      # SQL database to connect to 
 $SqlAuthLogin = 'ravi'          # SQL Authentication login
 $SqlAuthPw    = 'Freak@502'     # SQL Authentication login password
@@ -19,5 +19,5 @@ WHERE dec.session_id = @@SPID
 # sql authentication without database name
 $sqlservers = Get-Content -Path "C:\Users\ravi\Desktop\sql-servers.txt"
 Foreach($server in $sqlservers) {
-Invoke-Sqlcmd -ServerInstance $Server -U $SqlAuthLogin -P $SqlAuthPw -Query $Query | Format-Table
+Invoke-Sqlcmd -ServerInstance $server -U $SqlAuthLogin -P $SqlAuthPw -Query $Query | Format-Table
 }
